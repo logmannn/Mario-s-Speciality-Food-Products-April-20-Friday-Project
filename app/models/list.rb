4f3d6fde7717a64886fb9cd229da72fee7659871
Guest User <Guest@Epicodus-5C.local>
2018-04-19 09:44:06 -0700

    scope 1/3

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,4 +3,13 @@ class List < ActiveRecord::Base
   validates :name, :presence => true
   validates :image, :presence => true
   validates :description, :presence => true
+
+  scope :most_tasks, -> {(
+    select("lists.id, lists.name, lists.image, lists.description, lists.githublink, lists.livelink, count(tasks.id) as tasks_count")
+    .joins(:tasks)
+    .group("lists.id")
+    .order("tasks_count DESC")
+    .limit(10)
+  )}
+
 end
