d4540a33d34b3fc5f2d2570a4e2a56dc872e375b
Guest User <Guest@Epicodus-5C.local>
2018-04-19 09:20:11 -0700

    faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+2.times do |index|
+  List.create!(name: Faker::Book.title,
+                image: "https://increasify.com.au/wp-content/uploads/2016/08/default-image.png",
+                description: Faker::Lorem.sentence(20, false, 0).chop,
+                githublink: "https://github.com/NickoDelaCruz",
+                livelink: "https://www.youtube.com/")
+end
--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -4,6 +4,5 @@ describe List do
   it { should validate_presence_of :name }
   it { should validate_presence_of :image }
   it { should validate_presence_of :description }
-  it { should validate_presence_of :githublink }
   it { should have_many :tasks }
 end
