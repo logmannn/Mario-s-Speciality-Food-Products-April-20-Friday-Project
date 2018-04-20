0ab7e919c540c6949c05c365937fce1ad1b6f42a
lt <logantanous@gmail.com>
2018-04-20 12:05:07 -0700

    work on bug where textarea can't be filled in

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -3,12 +3,11 @@ require 'rails_helper'
 describe "the add a product process" do
   it "adds a new product" do
     visit products_path
-    click_link 'ADD a post'
-    fill_in 'Name', :with => 'Home stuff'
-    fill_in 'Image', :with => 'Home stuff'
-    fill_in 'Description', :with => 'Home stuff'
-    fill_in 'Githublink', :with => 'Home stuff'
-    fill_in 'Livelink', :with => 'Home stuff'
+    click_link 'ADD a Product'
+    fill_in 'Name', :with => 'Bob Jones'
+    fill_in 'Image', :with => 'https://i.imgur.com/SuI4cPh.gif'
+    fill_in 'country_of_origin', :with => 'USA'
+    fill_in 'Cost', :with => '3'
     click_on 'Create Product'
     expect(page).to have_content 'Products'
   end
