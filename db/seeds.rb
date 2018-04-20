3a27e89afe9ee23d5f0880c6dcf932fce24d7414
lt <logantanous@gmail.com>
2018-04-20 15:34:23 -0700

    get stars to look good

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,13 +2,13 @@
   product = Product.create!(
     name: Faker::Book.title,
     image: "https://increasify.com.au/wp-content/uploads/2016/08/default-image.png",
-    countryoforigin: Faker::StarWars.planet.chop,
+    countryoforigin: Faker::StarWars.planet,
     cost: rand(0..10000)
   )
   5.times do |index|
     product.reviews.create(
-      author: Faker::Name.unique.name.chop,
-      content_body: Faker::Lorem.words(20),
+      author: Faker::Name.unique.name,
+      content_body: Faker::Lorem.sentences(4).join(" "),
       rating: rand(0..5)
     )
   end
