a6718f3bf1a3c2e15e995fc8b0c4f3d1d0e2e832
lt <logantanous@gmail.com>
2018-04-20 14:02:25 -0700

    work on trying to figure out nested seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,12 @@
   Product.create!(name: Faker::Book.title,
                 image: "https://increasify.com.au/wp-content/uploads/2016/08/default-image.png",
                 countryoforigin: Faker::Lorem.sentence(20, false, 0).chop,
-                cost: 55)
+                cost: 55,
+                rewiews: [
+                  {
+                    author: 'Q1',
+                    
+                  }
+                ]
+              )
 end
