8ef403ed8aa306a4e82bb32b701420b48fc4bb6b
lt <logantanous@gmail.com>
2018-04-20 11:46:29 -0700

    review content body must stay between 50 and 250 characters

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,10 @@ class Review < ActiveRecord::Base
 
   validates :author, :presence => true
   validates :content_body, :presence => true
+  validates :content_body, length: { maximum: 250,
+    too_long: "%{count} characters is the maximum allowed" }
+    validates :content_body, length: { minimum: 50,
+      too_short: "%{count} characters is the minimum allowed" }    
   validates :rating, :presence => true
   validates :rating, :numericality => { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
 end
