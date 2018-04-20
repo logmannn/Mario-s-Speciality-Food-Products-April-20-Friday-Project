87a17f26bc924e87980ecf0f12a1d278445552ef
lt <logantanous@gmail.com>
2018-04-20 14:35:34 -0700

    get done neccessary functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
-2.times do |index|
+50.times do |index|
   product = Product.create!(
     name: Faker::Book.title,
     image: "https://increasify.com.au/wp-content/uploads/2016/08/default-image.png",
     countryoforigin: Faker::StarWars.planet.chop,
-    cost: 55
+    cost: rand(0..10000)
   )
-
-  product.reviews.create(
-    author: Faker::Name.unique.name.chop,
-    content_body: Faker::Lorem.characters(55),
-    rating: 5
-  )
-  
+  5.times do |index|
+    product.reviews.create(
+      author: Faker::Name.unique.name.chop,
+      content_body: Faker::Lorem.characters(55),
+      rating: rand(0..5)
+    )
+  end
 end
