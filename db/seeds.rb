18e7345ee8d80acee6294ba71bda102c7afdb08b
lt <logantanous@gmail.com>
2018-04-20 14:25:53 -0700

    get nested seeding to work

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,15 @@
 2.times do |index|
-  Product.create!(name: Faker::Book.title,
-                image: "https://increasify.com.au/wp-content/uploads/2016/08/default-image.png",
-                countryoforigin: Faker::Lorem.sentence(20, false, 0).chop,
-                cost: 55,
-                rewiews: [
-                  {
-                    author: 'Q1',
-                    
-                  }
-                ]
-              )
+  product = Product.create!(
+    name: Faker::Book.title,
+    image: "https://increasify.com.au/wp-content/uploads/2016/08/default-image.png",
+    countryoforigin: Faker::StarWars.planet.chop,
+    cost: 55
+  )
+
+  product.reviews.create(
+    author: Faker::Name.unique.name.chop,
+    content_body: Faker::Lorem.characters(55),
+    rating: 5
+  )
+  
 end
